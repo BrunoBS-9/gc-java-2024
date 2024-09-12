845adac52996265218440e48612dc19fdeda389e
alunojava-35 <alunojava-35@MacBook-Pro-de-Adminitrador.local>
2024-09-12 17:29:24 -0300

    commitando teste

---
+++ Aula2/src/Aluno.java
@@ -0,0 +1,13 @@
+public class Aluno {
+    private String aluno;
+    private Integer idade;
+    private String matricula;
+
+    public Aluno(String aluno, Integer idade, String matricula) {
+        this.aluno = aluno;
+        this.idade = idade;
+        this.matricula = matricula;
+    }
+
+
+}
