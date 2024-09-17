32240760771f52dac71ec20bf67bc3de57f7f264
BrunoBitencourt99 <bruno.bitencourt.silva@gmail.com>
2024-09-17 16:54:24 -0300

    Conclusion sum activity

---
+++ src/Aula 1/Exercicio  2/soma.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Diga uma soma");
+    System.out.println("Primeiro Número:");
+    int numero1 = Integer.parseInt(scanner.nextLine());
+    System.out.println("Segundo Número:");
+    int numero2 = Integer.parseInt(scanner.nextLine());
+
+    System.out.println(STR."A sua soma é \{numero1 + numero2}");
+}
