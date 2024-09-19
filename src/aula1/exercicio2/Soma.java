8ca173f1ed9dfff3d8f6bf5547c58d58d71b8509
BrunoBitencourt99 <bruno.bitencourt.silva@gmail.com>
2024-09-19 15:54:02 -0300

    Ajustes no Mac e Inicio aula2.exercicio1

--- src/aula1/exercicio2/Soma.java
+++ src/aula1/exercicio2/Soma.java
@@ -1,5 +1,7 @@
 import java.util.Scanner;
 
+import static java.lang.StringTemplate.STR;
+
 public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
---
+++ src/aula2/exercicio1/NumerosPrimos.java
@@ -0,0 +1,20 @@
+package aula2.exercicio1;
+import java.util.Scanner;
+
+public class NumerosPrimos {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //limite números primos
+        int limite = 100;
+
+        //Adquirindo espassamento
+        System.out.println("Diga entre quais numeros quer o espassamento");
+        System.out.println("Começa em");
+        int numeroInicial = Integer.parseInt(scanner.nextLine());
+        System.out.println("Número Final");
+        int numeroFinal = Integer.parseInt(scanner.nextLine());
+
+
+    }
+}
