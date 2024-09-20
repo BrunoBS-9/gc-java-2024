593ed6131ca32f7916caec391a024828a0aae4b7
Bruno Bitencourt da Silva <bruno.bitencourt.silva@gmail.com>
2024-09-20 19:25:40 -0300

    aula3.exercicio1 - finalizado

---
+++ src/aula3/exercicio1/Carro.java
@@ -0,0 +1,15 @@
+package aula3.exercicio1;
+
+public class Carro extends Veiculos {
+        double consumo = 0.20;
+
+        public Carro(String modelo,String marca,int ano) {
+                super(modelo, marca, ano);
+        }
+
+        @Override
+        public double calcularCustoViagem(int distancia) {
+        return consumo * distancia;
+        }
+    }
+
