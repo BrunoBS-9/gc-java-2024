0e8ccae3fa405a3da9c7df00fd8a375927f4f472
Helio Fabricio <heliofabricio.11@gmail.com>
2024-09-22 16:04:35 -0300

    adicionando nomes

--- src/Aula02/Exercicio07/ExercicioA02E07.java
+++ src/Aula02/Exercicio07/ExercicioA02E07.java
@@ -7,3 +7,16 @@ public class ExercicioA02E07 {
         menu.mostrarMenu();
     }
 }
+
+/*
+
+    Colaboradores:
+    Nome: Marisol Carvalho Alves Marques
+    Email: solcamarques@outlook.com
+    Github: https://github.com/SolcaMarques
+
+    Nome: Hélio Fabricio Escobar Pereira do Amaral Prates
+    Email: heliofabricio.11@gmail.com
+    Github: https://github.com/HelioFabricio/
+
+ */
