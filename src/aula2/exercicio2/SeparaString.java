1e068b00d59d95b9f47101d2ca454e08e77083e4
Bruno Bitencourt da Silva <bruno.bitencourt.silva@gmail.com>
2024-09-22 21:43:07 -0300

    aula2 exercicio2 - finalizado

--- src/aula2/exercicio2/SeparaString.java
+++ src/aula2/exercicio2/SeparaString.java
@@ -1,2 +1,21 @@
-package aula2.exercicio2;public class SeparaString {
+package aula2.exercicio2;
+import java.util.Scanner;
+
+public class SeparaString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Palavra base
+        System.out.println("Diga a maior palavra que conhece");
+        String maiorPalavra = scanner.nextLine();
+
+        //separar String
+        int separar = maiorPalavra.length() /2;
+
+        //Declarar as partes
+        String metade1 = maiorPalavra.substring(0, separar);
+        String metade2 = maiorPalavra.substring(separar);
+
+        System.out.println(metade1);
+    }
 }
