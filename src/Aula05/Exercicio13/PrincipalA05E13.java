718442927768c50f2afd6a342fed25947d0e35b2
BrunoBitencourt99 <bruno.bitencourt.silva@gmail.com>
2024-09-24 17:16:13 -0300

    Começo atividade 13

--- src/Aula05/Exercicio13/PrincipalA05E13.java
+++ src/Aula05/Exercicio13/PrincipalA05E13.java
@@ -1,7 +1,24 @@
 package Aula05.Exercicio13;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class PrincipalA05E13 {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        ArrayList<String> listaCompras = new ArrayList<>();
+
+        int escolha;
+
+        do {
+
+            System.out.println("Quer iniciar a lista de compras?");
+            escolha = scan.nextInt();
+
+            switch (){
+
+            }
+        } while (escolha != 0);
     }
 }
