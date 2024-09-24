4cd44a4f3051bf807b324c16311be3c3bd41ac16
alunojava-35 <alunojava-35@MacBook-Pro-de-Adminitrador.local>
2024-09-24 17:15:51 -0300

    update adicionando classes basicas do projeto

---
+++ src/Aula05/Exercicio14/Caminhao.java
@@ -0,0 +1,12 @@
+package Aula05.Exercicio14;
+
+public class Caminhao implements  Veiculo{
+    private String marca;
+    private String modelo;
+    private int ano;
+    private int maximoDeToneladas;
+    @Override
+    public String coletarDetalhes() {
+        return null;
+    }
+}
---
+++ src/Aula05/Exercicio14/Carro.java
@@ -0,0 +1,13 @@
+package Aula05.Exercicio14;
+
+public class Carro implements Veiculo{
+    private String marca;
+    private String modelo;
+    private int ano;
+    private int quantidadeDePortas;
+
+    @Override
+    public String coletarDetalhes(){
+        return null;
+    }
+}
