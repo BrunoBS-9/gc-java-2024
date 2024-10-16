991951f0ea30a8373a5fdf1c623b625b09c48cc3
Bruno Bitencourt da Silva <bruno.bitencourt.silva@gmail.com>
2024-10-16 19:58:01 -0300

    Exercicio23 da aula07 - finalizado

--- src/aula07/exercicio21/Main.java
+++ src/aula07/exercicio21/Main.java
@@ -1,14 +1,20 @@
 package aula07.exercicio21;
+
 import java.util.Scanner;
 
-public class ExceptionMain extends ErroNumNegativo{
+public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Diga um número");
         int numero = scan.nextInt();
 
-        try {verificarNumero(numero);
+        try {
+            if (numero < 0) {
+                throw new IllegalArgumentException("O número não pode ser negativo");
+            }
+
+            System.out.println("O número é válido: " + numero);
         } catch (IllegalArgumentException e) {
             System.out.println("Exceção capturada: " + e.getMessage());
         }
--- src/aula07/exercicio22/Main.java
+++ src/aula07/exercicio22/Main.java
@@ -1,20 +1,25 @@
 package aula07.exercicio22;
 import java.util.Scanner;
 
-public class ExceptionMain extends ValorInvalidoException{
-    public ExceptionMain(String mensage) {
-        super(mensage);
-    }
-
+public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Diga um número");
         int numero = scan.nextInt();
 
-        try {verificarNumero(numero);
+        try {
+            verificarNumero(numero);
         } catch (ValorInvalidoException e) {
             System.out.println("Exceção capturada: " + e.getMessage());
         }
+    }
+
+    public static void verificarNumero(int numero) {
+        if (numero <= 10) {
+            throw new ValorInvalidoException("O número não pode ser menor que 10");
+        }
+
+        System.out.println("O número é válido: " + numero);
     }
 }
