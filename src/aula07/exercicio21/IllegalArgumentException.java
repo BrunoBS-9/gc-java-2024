c388aef4409f0a611c0897cb824bf71b53df6ecf
Bruno Bitencourt da Silva <135754345+BrunoBitencourt99@users.noreply.github.com>
2024-10-15 22:28:22 -0300

    Update IllegalArgumentException.java

--- src/aula07/exercicio21/IllegalArgumentException.java
+++ src/aula07/exercicio21/IllegalArgumentException.java
@@ -1,7 +1,1 @@
-package aula07.exercicio21;
 
-public class IllegalArgumentException extends RuntimeException {
-    public IllegalArgumentException(String mensage) {
-        super(mensage);
-    }
-}
